48816309c66c196db6b52b70d090b2588e1c2a62
Austin Downing <HanSolo552@gmail.com>
2018-10-29 10:22:21 -0400

    Add files via upload

---
+++ Delete Snagit Files.ps1
@@ -0,0 +1,11 @@
+﻿#Location Of The Datastore for Each Profile
+$FileLocationDataStore = "\\fileshare\Profiles\*\AppData\Roaming\TechSmith\SnagIt\DataStore\*"
+
+#Location Of The Thumbnails for each profile
+$FileLocationThumbnails = "\\fileshare\Profiles\*\AppData\Roaming\TechSmith\SnagIt\SnagIt\Thumbnails\*"
+
+#Set date limit for how long you would like to keep files
+$datelimit = (Get-Date).AddDays(-30)
+
+Get-ChildItem $FileLocationDataStore -Force | Where-Object {$_.LastWriteTime -lt $datelimit} | Remove-Item -Include *.snag, *.snagundo -Force -confirm:$false -Recurse
+Get-ChildItem $FileLocationThumbnails -Force | Where-Object {$_.LastWriteTime -lt $datelimit} | Remove-Item -Include *.thumb -Force -confirm:$false -Recurse
